60f07745d63f3075d8b82718b327172fd2bf381d
FJJLeon <695048014@qq.com>
2020-03-22 00:35:12 +0800

    update 448, in-place O(N) sort

--- 448. Find All Numbers Disappeared in an Array/solution.cpp
+++ 448. Find All Numbers Disappeared in an Array/solution.cpp
@@ -32,12 +32,20 @@ public:
  * 
  * there are several tricks:
  * a. for each position i, mark the value of index **nums[i]-1** as negative in first loop, and then check those positive
- * Runtime: 120 ms, faster than 40.54%
- * Memory Usage: 12.6 MB, less than 100.00%
+ * Runtime: 120 ms, faster than 40.54%, O(N)
+ * Memory Usage: 12.6 MB, less than 100.00%, O(1)
  * 
  * b. for each position i, add N to the value of index **nums[i]%N-1** in first loop, and then check less then or equal to N
- * Runtime: 116 ms, faster than 61.35%
- * Memory Usage: 12.6 MB, less than 100.00% 
+ * Runtime: 116 ms, faster than 61.35%, O(N)
+ * Memory Usage: 12.6 MB, less than 100.00%, O(1) 
+ * 
+ * c. we can make in-place sort in O(N) time because the fact
+ * sort result: nums[i] - 1 = i
+ * for each index i, if nums[i] != nums[nums[i]-1], swap the two.
+ * nums[i] != nums[nums[i]-1] means nums[i] is not in the right position(index = nums[i]-1), swap to make nums[i] in right place
+ * the time complexity is O(N), each value will be visited and swaped to the right place at most once
+ * Runtime: 120 ms, faster than 40.54%, O(N)
+ * Memory Usage: 12.8 MB, less than 100.00%, O(1)
  * 
  * the problem is the input array modified
  * 
@@ -71,6 +79,25 @@ public:
             if (nums[i] <= N)
                 res.push_back(i+1);
         }
+        return res;
+    }
+};
+
+class Solution3c {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            // nums[i] is not in the right position(index = nums[i]-1), swap
+            // the time complexity is O(N), each value will be visited and swaped to the right place at most once
+            while (nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        
+        vector<int> res;
+        for (int i = 0; i < nums.size(); ++i)
+            if (nums[i] != i+1) 
+                res.push_back(i+1);
         return res;
     }
 };
